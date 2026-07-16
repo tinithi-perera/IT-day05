bd6eb4e48fc22bcdeaf534aa0969ec5698b5127c
tinithi-perera <tinithiperera@gmail.com>
2026-07-16 23:02:37 +0530

    Refactor app.js to uncomment code for clarity and maintain structure

--- js/app.js
+++ js/app.js
@@ -1,34 +1,60 @@
 
-    console.log("hello tinna");
-    let name="tinithi";
-    name="avishka";
-    console.log( name);
-    alert("hello Tinithi");
-    // alert("Hello tinithi");
-    // let Isafteral=false;
-    // console.log( typeof Isafteral);
+console.log("hello tinna");
+let name = "tinithi";
+name = "avishka";
+console.log(name);
+alert("hello Tinithi");
+// alert("Hello tinithi");
+// let Isafteral=false;
+// console.log( typeof Isafteral);
 
-    // let x=10;
-    // let y="10";
+// let x=10;
+// let y="10";
 
-    // console.log(x==y);
-    
-    // if(true||false){
-    //     console.log("true");
-    // }
-    // else{
-    //     console.log("false");
-    // }
-        
-    // for(let i=1;i<=10;i++){
-    //     console.log(i);   
-    // }
-    
-    // let i=0;
-    // while(i<=10){
-    //     console.log(i);
-    //     i++; 
-    // }
-    
-    
+// console.log(x==y);
 
+// if(true||false){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// for loop
+// for(let i=1;i<=10;i++){
+//     console.log(i);   
+// }
+
+// while loop
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++; 
+// }
+
+// // functions
+// function add(x,y){
+//     return x+y;
+// }
+// let result=add(10,20);
+// console.log(result);
+
+// arrays
+// let numbers = [1, 2, 3, 4, 5, 6];
+// for (let i = 0; i <= numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// let array = [1, "kamindu", 3, "tinithi", 5, 6];
+// for (let i = 0; i <array.length; i++) {
+//     console.log(array[i]);
+// }
+
+// objects
+// let customer={
+//     name:"tinithi",
+//     age: 18,
+//     address: "moratuwa"
+// }
+ 
+// console.log(customer);
