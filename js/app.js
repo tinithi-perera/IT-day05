e8ffe851554ce4b9c265f5c92f86fb864603c627
tinithi-perera <tinithiperera@gmail.com>
2026-07-16 22:06:24 +0530

    Refactor app.js to uncomment code for clarity and enhance loop functionality

--- js/app.js
+++ js/app.js
@@ -8,11 +8,27 @@
     // let Isafteral=false;
     // console.log( typeof Isafteral);
 
-    let x=10;
-    let y="10";
+    // let x=10;
+    // let y="10";
 
-    console.log(x==y);
+    // console.log(x==y);
     
+    // if(true||false){
+    //     console.log("true");
+    // }
+    // else{
+    //     console.log("false");
+    // }
+        
+    // for(let i=1;i<=10;i++){
+    //     console.log(i);   
+    // }
+    
+    let i=0;
+    while(i<=10){
+        console.log(i);
+        i++; 
+    }
     
     
 
