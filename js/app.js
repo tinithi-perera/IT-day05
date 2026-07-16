fa251b8b61fcf745c5b20640844697169dbe131e
tinithi-perera <tinithiperera@gmail.com>
2026-07-16 20:27:00 +0530

    Refactor app.js to clean up commented code and maintain variable declarations

--- js/app.js
+++ js/app.js
@@ -4,5 +4,15 @@
     name="avishka";
     console.log( name);
     alert("hello Tinithi");
+    // alert("Hello tinithi");
+    // let Isafteral=false;
+    // console.log( typeof Isafteral);
+
+    let x=10;
+    let y="10";
+
+    console.log(x==y);
+    
+    
     
 
