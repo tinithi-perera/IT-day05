93455daa1923fd3b393c34ba66c57aa9544a22e6
tinithi-perera <tinithiperera@gmail.com>
2026-07-16 22:06:38 +0530

    Refactor app.js to comment out while loop for future use

--- js/app.js
+++ js/app.js
@@ -24,11 +24,11 @@
     //     console.log(i);   
     // }
     
-    let i=0;
-    while(i<=10){
-        console.log(i);
-        i++; 
-    }
+    // let i=0;
+    // while(i<=10){
+    //     console.log(i);
+    //     i++; 
+    // }
     
     
 
